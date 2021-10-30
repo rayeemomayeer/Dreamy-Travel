6d11a3a9173c43ffb5f3d1e4cd7ba5c774387fd5
Rayeem Omayeer <rayeemomayeer@gmail.com>
2021-10-31 02:17:30 +0600

    tours data updated

--- src/components/Home/Tours/Tours.js
+++ src/components/Home/Tours/Tours.js
@@ -13,7 +13,7 @@ const Tours = () => {
   <div id="tours" className="bg-white">
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       
-      <h1 className="title-font sm:text-4xl text-3xl m-16 font-bold text-gray-900" style={{fontFamily: "Montserrat, sans-serif"}}>Book your dream vacation now</h1>
+      <h1 className="title-font sm:text-4xl text-3xl my-16 font-bold text-gray-900 text-center" style={{fontFamily: "Montserrat, sans-serif"}}>Book your dream vacation now</h1>
 
       <div className="mt-6 grid grid-cols-1 gap-y-16 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {
