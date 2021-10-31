84f65056ead88865ecd01f9bba3b9b366ece39e7
Rayeem Omayeer <rayeemomayeer@gmail.com>
2021-10-31 22:48:41 +0600

    updated

--- src/components/Home/TopBanner/TopBanner.js
+++ src/components/Home/TopBanner/TopBanner.js
@@ -13,21 +13,21 @@ const TopBanner = () => {
     <div className="carousel-item active">
       <img className="object-cover w-full" src="https://images.unsplash.com/photo-1492294112339-ea831887e5d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80" className="d-block w-100" alt="..." />
       <div className="carousel-caption d-none d-md-block">
-        <h1 className="text-8xl font-serif text-green-900">First slide label</h1>
+        <h1 className="text-8xl font-serif text-pink-900">Explore The World For Yourself</h1>
         <p className="text-xl">Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div className="carousel-item">
       <img className="object-cover w-full" src="https://images.unsplash.com/photo-1623678254109-e7eb91f97bf4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1376&q=80" className="d-block w-100" alt="..." />
       <div className="carousel-caption d-none d-md-block">
-        <h5 className="text-8xl font-serif  text-yellow-500">Second slide label</h5>
+        <h5 className="text-8xl font-serif  text-yellow-500">Book your dream vacation now</h5>
         <p className="text-xl">Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div className="carousel-item">
       <img className="object-cover w-full" src="https://images.unsplash.com/photo-1600520611035-84157ad4084d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80" className="d-block w-100" alt="..." />
       <div className="carousel-caption d-none d-md-block">
-        <h5 className="text-8xl font-serif  text-yellow-900">Third slide label</h5>
+        <h5 className="text-8xl font-serif  text-yellow-900">Services From Our Hotels</h5>
         <p className="text-xl">Some representative placeholder content for the third slide.</p>
       </div>
     </div>
