9acd67313873583ca461beea1873c8b10c6dcad1
Rayeem Omayeer <rayeemomayeer@gmail.com>
2021-10-31 20:30:06 +0600

    updated

--- src/components/PlaceOrder/PlaceOrder.js
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -44,7 +44,7 @@ const PlaceOrder = () => {
         <div className="max-w-sm mx-auto space-y-5 md:w-2/3">
           <div>
             <div className=" relative ">
-              <input required type="text" id="user-info-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name" />
+              <input value={user?.displayName} required type="text" id="user-info-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name" />
             </div>
           </div>
           <div>
